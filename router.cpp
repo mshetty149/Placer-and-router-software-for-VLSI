a34dea54e74c3f5754b824f786a7532f36bf592a
Manish Shetty <mshetty149@gmail.com>
2020-04-07 21:33:47 +0530

    Small changes in router.cpp

--- router.cpp
+++ router.cpp
@@ -93,213 +93,222 @@ int main(){
 	//Input Files (Grid)
 	
 	ifstream fin(gridfile);
-	if(fin.is_open()){
-		fin>>x_size>>y_size;
-		fin>>bendpenalty>>viapenalty;
-		cell grid[2][x_size][y_size];
-		bool via[x_size][y_size];
-		for(int l_no=0;l_no<2;l_no++){
-			for(int j=0;j<y_size;j++){
-				for(int i=0;i<x_size;i++){
-					fin>>grid[l_no][i][j].cost;
-					via[i][j]=false;
-				}
-	    	}
-    	}	
-    	fin.close();
-    //Netlist
-
-    	int nnz,lno;
-    	ifstream fnin(netlistfile);
-    	fnin>>nnz;
-    	net in_nets[nnz];
-    	int i_lk;
-    	for(int i=0;i<nnz;i++){
-    		fnin>>i_lk;
-    		for(int i_l=0;i_l<2;i_l++){
-    			
-    			fnin>>lno;
-				in_nets[i].lay_no[i_l]=(lno==1)?false:true;
-    			fnin>>in_nets[i].x[i_l]>>in_nets[i].y[i_l];
-    			grid[lno][in_nets[i].x[i_l]][in_nets[i].y[i_l]].is_routed=true;
+	if(!fin.is_open()){
+		cout<<"Unable to open file"<<endl;
+		return -1;
+	}
+	fin>>x_size>>y_size;
+	fin>>bendpenalty>>viapenalty;
+	cell grid[2][x_size][y_size];
+	bool via[x_size][y_size];
+	for(int l_no=0;l_no<2;l_no++){
+		for(int j=0;j<y_size;j++){
+			for(int i=0;i<x_size;i++){
+				fin>>grid[l_no][i][j].cost;
+				via[i][j]=false;
 			}
-		}
-		fnin.close();	
-		//One layer unit cost router
-		vector <wave_obj> netout[nnz];  
-		
-		for(int i=0;i<nnz;i++){
-			cout<<i+1<<endl;
-			wave_obj w_src,w_tar;
-			//declare source and target of net
-			w_src.lay_no=in_nets[i].lay_no[0];
-			w_src.x=in_nets[i].x[0];
-			w_src.y=in_nets[i].y[0];
-			w_src.pathcost=1;
-		
-			grid[w_src.lay_no][w_src.x][w_src.y].is_reached=true;
-//			if(grid[w_src.lay_no][w_src.x][w_src.y].cost<0) grid[w_src.lay_no][w_src.x][w_src.y].cost=1;
+    	}
+	}	
+	fin.close();
+//Netlist
+
+	int nnz,lno;
+	ifstream fnin(netlistfile);
+	
+	if(!fnin.is_open()){
+		cout<<"Unable to open file"<<endl;
+		return -1;
+	}
+	
+	fnin>>nnz;
+	net in_nets[nnz];
+	int i_lk;
+	for(int i=0;i<nnz;i++){
+		fnin>>i_lk;
+		for(int i_l=0;i_l<2;i_l++){
 			
-			w_tar.lay_no=in_nets[i].lay_no[1];
-			w_tar.x=in_nets[i].x[1];
-			w_tar.y=in_nets[i].y[1];
+			fnin>>lno;
+			in_nets[i].lay_no[i_l]=(lno==1)?false:true;
+			fnin>>in_nets[i].x[i_l]>>in_nets[i].y[i_l];
+			grid[lno][in_nets[i].x[i_l]][in_nets[i].y[i_l]].is_routed=true;
+		}
+	}
+	fnin.close();	
+	//One layer unit cost router
+	vector <wave_obj> netout[nnz];  
+	
+	for(int i=0;i<nnz;i++){
+		cout<<i+1<<endl;
+		wave_obj w_src,w_tar;
+		//declare source and target of net
+		w_src.lay_no=in_nets[i].lay_no[0];
+		w_src.x=in_nets[i].x[0];
+		w_src.y=in_nets[i].y[0];
+		w_src.pathcost=1;
+	
+		grid[w_src.lay_no][w_src.x][w_src.y].is_reached=true;
+//			if(grid[w_src.lay_no][w_src.x][w_src.y].cost<0) grid[w_src.lay_no][w_src.x][w_src.y].cost=1;
+		
+		w_tar.lay_no=in_nets[i].lay_no[1];
+		w_tar.x=in_nets[i].x[1];
+		w_tar.y=in_nets[i].y[1];
 
-			grid[w_tar.lay_no][w_tar.x][w_tar.y].is_routed=false;
-			cout<<"Source "<<w_src.x<<" "<<w_src.y<<" "<< w_src.lay_no<<endl;
-			cout<<"Target "<<w_tar.x<<" "<<w_tar.y<<" "<< w_tar.lay_no<<endl;
-			//expand
-			priority_queue <wave_obj> wavefront;
-			int mi=0;
-			wavefront.push(w_src);
+		grid[w_tar.lay_no][w_tar.x][w_tar.y].is_routed=false;
+		cout<<"Source "<<w_src.x<<" "<<w_src.y<<" "<< w_src.lay_no<<endl;
+		cout<<"Target "<<w_tar.x<<" "<<w_tar.y<<" "<< w_tar.lay_no<<endl;
+		//expand
+		priority_queue <wave_obj> wavefront;
+		int mi=0;
+		wavefront.push(w_src);
 
-			bool target_found=false;
-			bool notpossible=false;
-			while(!target_found){
-				if(wavefront.empty()){
-					cout<<"No path possible for net number " << i+1 <<endl;
-					notpossible=true;
-					break;
-			    }
-			    //Expansion
-			    enum pred pred_temp; 
-				vector<enum pred> neigh;
-			    wave_obj w= wavefront.top();
-			   //neighbours
-			    int x_ng[]={-1,1,0,0};
-			    int y_ng[]={0,0,-1,1};
-			    enum pred p_ng[]={E,W,N,S};
-			    
-		        for(int ip=0;ip<4;ip++){
+		bool target_found=false;
+		bool notpossible=false;
+		while(!target_found){
+			if(wavefront.empty()){
+				cout<<"No path possible for net number " << i+1 <<endl;
+				notpossible=true;
+				break;
+		    }
+		    //Expansion
+		    enum pred pred_temp; 
+			vector<enum pred> neigh;
+		    wave_obj w= wavefront.top();
+		   //neighbours
+		    int x_ng[]={-1,1,0,0};
+		    int y_ng[]={0,0,-1,1};
+		    enum pred p_ng[]={E,W,N,S};
+		    
+	        for(int ip=0;ip<4;ip++){
 //		        	cout<<grid[w.lay_no][w.x+x_ng[ip]][w.y+y_ng[ip]].is_routed<<endl;
-					int x_tmp=w.x+x_ng[ip];
-					int y_tmp=w.y+y_ng[ip];
-					if(x_tmp<x_size&&x_tmp>=0&&y_tmp<y_size&&y_tmp>=0)
-					    if(!grid[w.lay_no][x_tmp][y_tmp].is_routed&&!grid[w.lay_no][x_tmp][y_tmp].is_reached&&!grid[w.lay_no][x_tmp][y_tmp].is_blocked())
-					   		neigh.push_back(p_ng[ip]);
-				}
-				if(!grid[!w.lay_no][w.x][w.y].is_routed&&!grid[!w.lay_no][w.x][w.y].is_reached&&!grid[!w.lay_no][w.x][w.y].is_blocked())
-					if(w.lay_no)
-						neigh.push_back(up);
-					else
-						neigh.push_back(down);
+				int x_tmp=w.x+x_ng[ip];
+				int y_tmp=w.y+y_ng[ip];
+				if(x_tmp<x_size&&x_tmp>=0&&y_tmp<y_size&&y_tmp>=0)
+				    if(!grid[w.lay_no][x_tmp][y_tmp].is_routed&&!grid[w.lay_no][x_tmp][y_tmp].is_reached&&!grid[w.lay_no][x_tmp][y_tmp].is_blocked())
+				   		neigh.push_back(p_ng[ip]);
+			}
+			if(!grid[!w.lay_no][w.x][w.y].is_routed&&!grid[!w.lay_no][w.x][w.y].is_reached&&!grid[!w.lay_no][w.x][w.y].is_blocked())
+				if(w.lay_no)
+					neigh.push_back(up);
+				else
+					neigh.push_back(down);
 //				for(int neif=0;neif<neigh.size();neif++)
 //				cout<<neigh[neif]<<endl;
 //				cout<<neigh.size()<<endl;
-				for(int n_i=0;n_i<neigh.size();n_i++){
-					
-					wave_obj w_temp;
-					w_temp=w;
-					w_temp.pred_cell=neigh[n_i];
-					switch(neigh[n_i]){
-						case N:
-							w_temp.y--;
-							break;
-						case S:
-							w_temp.y++;
-							break;
-						case E:
-							w_temp.x--;
-							break;
-						case W:
-							w_temp.x++;
-							break;
-						case up: case down:
-							w_temp.lay_no=!w_temp.lay_no;
-							w_temp.pathcost+=viapenalty;
-							break;
-					}
-//					cout<<w_temp.lay_no<<" "<<w_temp.x<<" "<<w_temp.y<<endl;
-				//	int bias=bbox(w_temp.x,w_temp.y,w_tar.x,w_tar.y);
-				//	bool givebias=false;
-					w_temp.pathcost+=grid[w_temp.lay_no][w_temp.x][w_temp.y].cost;	
-					if(isbendpresent(w_temp,w,w_src))
-						w_temp.pathcost+=bendpenalty;
-					wavefront.push(w_temp);
-				//	if(givebias)w_temp.pathcost+=bias;
-				//Save the info in the grid itself
-					grid[w_temp.lay_no][w_temp.x][w_temp.y].pred_cell=w_temp.pred_cell;
-					grid[w_temp.lay_no][w_temp.x][w_temp.y].is_reached=true;
-//					cout<<w_temp.x<<" "<<w_temp.y<<" "<<w_temp.lay_no<<" "<<w_temp.pred_cell<<endl;
-					if(istarget_reached(w_tar,w_temp)){
-						w_tar.pred_cell=w_temp.pred_cell;
-						w_tar.pathcost=w_temp.pathcost;
-						target_found=true;
+			for(int n_i=0;n_i<neigh.size();n_i++){
+				
+				wave_obj w_temp;
+				w_temp=w;
+				w_temp.pred_cell=neigh[n_i];
+				switch(neigh[n_i]){
+					case N:
+						w_temp.y--;
 						break;
-					}
-					
+					case S:
+						w_temp.y++;
+						break;
+					case E:
+						w_temp.x--;
+						break;
+					case W:
+						w_temp.x++;
+						break;
+					case up: case down:
+						w_temp.lay_no=!w_temp.lay_no;
+						w_temp.pathcost+=viapenalty;
+						break;
 				}
-	//			if(mi>30) break;
-	//					mi++;
-				wavefront.pop();
-					
+//					cout<<w_temp.lay_no<<" "<<w_temp.x<<" "<<w_temp.y<<endl;
+			//	int bias=bbox(w_temp.x,w_temp.y,w_tar.x,w_tar.y);
+			//	bool givebias=false;
+				w_temp.pathcost+=grid[w_temp.lay_no][w_temp.x][w_temp.y].cost;	
+				if(isbendpresent(w_temp,w,w_src))
+					w_temp.pathcost+=bendpenalty;
+				wavefront.push(w_temp);
+			//	if(givebias)w_temp.pathcost+=bias;
+			//Save the info in the grid itself
+				grid[w_temp.lay_no][w_temp.x][w_temp.y].pred_cell=w_temp.pred_cell;
+				grid[w_temp.lay_no][w_temp.x][w_temp.y].is_reached=true;
+//					cout<<w_temp.x<<" "<<w_temp.y<<" "<<w_temp.lay_no<<" "<<w_temp.pred_cell<<endl;
+				if(istarget_reached(w_tar,w_temp)){
+					w_tar.pred_cell=w_temp.pred_cell;
+					w_tar.pathcost=w_temp.pathcost;
+					target_found=true;
+					break;
+				}
+				
 			}
+//			if(mi>30) break;
+//					mi++;
+			wavefront.pop();
+				
+		}
 //			cout<<mi<<endl;
 //			cout<<grid1[119][31].cost<<endl;
-			cout<< " "<<w_tar.pathcost<<endl;
-			//backtrace
-			if(!notpossible){
-				wave_obj w_btr=w_tar;
+		cout<< " "<<w_tar.pathcost<<endl;
+		//backtrace
+		if(!notpossible){
+			wave_obj w_btr=w_tar;
+			grid[w_btr.lay_no][w_btr.x][w_btr.y].is_routed=true;
+			netout[i].push_back(w_btr);
+			int li=0;
+		
+			while(!istarget_reached(w_src,w_btr)){
+					
+				//cout<<w_btr.x<<" "<<w_btr.y<<" Layno "<<w_btr.lay_no<<" "<<w_btr.pred_cell<<endl;
+				switch(w_btr.pred_cell){
+					case N:
+						w_btr.y++;
+						break;
+					case S:
+						w_btr.y--;
+						break;
+					case E:
+						w_btr.x++;
+						break;
+					case W:
+						w_btr.x--;
+						break;
+					case up:case down:
+						w_btr.lay_no=!w_btr.lay_no;
+						via[w_btr.x][w_btr.y]=true;
+						break;
+				}	
+				w_btr.pred_cell=grid[w_btr.lay_no][w_btr.x][w_btr.y].pred_cell;
 				grid[w_btr.lay_no][w_btr.x][w_btr.y].is_routed=true;
 				netout[i].push_back(w_btr);
-				int li=0;
-			
-				while(!istarget_reached(w_src,w_btr)){
-						
-					//cout<<w_btr.x<<" "<<w_btr.y<<" Layno "<<w_btr.lay_no<<" "<<w_btr.pred_cell<<endl;
-					switch(w_btr.pred_cell){
-						case N:
-							w_btr.y++;
-							break;
-						case S:
-							w_btr.y--;
-							break;
-						case E:
-							w_btr.x++;
-							break;
-						case W:
-							w_btr.x--;
-							break;
-						case up:case down:
-							w_btr.lay_no=!w_btr.lay_no;
-							via[w_btr.x][w_btr.y]=true;
-							break;
-					}	
-					w_btr.pred_cell=grid[w_btr.lay_no][w_btr.x][w_btr.y].pred_cell;
-					grid[w_btr.lay_no][w_btr.x][w_btr.y].is_routed=true;
-					netout[i].push_back(w_btr);
-				}
 			}
-			//cleanup
-			for(int l_c=0;l_c<2;l_c++){
-				for(int i_c=0;i_c<x_size;i_c++){
-					for(int j_c=0;j_c<y_size;j_c++){
-						grid[l_c][i_c][j_c].is_reached=false;
-					}
-				}
-			}
-			cout<<"Route of net " <<i+1<<" completed"<<endl;
-			while(!wavefront.empty())
-				wavefront.pop();		
 		}
-		ofstream fout(outputfile);
-		
-		fout<<nnz<<endl;
-	
-		for(int i=0;i<nnz;i++){
-			fout<<i+1<<endl; 
-			if(netout[i].size()!=2){
-				for(int j=netout[i].size()-1;j>=0;j--){
-					fout<<netout[i][j].lay_no+1<<" "<<netout[i][j].x<<" "<<netout[i][j].y<<endl;
-					if(via[netout[i][j].x][netout[i][j].y]){
-						fout<< "3 "<<netout[i][j].x<<" "<<netout[i][j].y<<endl;
-						via[netout[i][j].x][netout[i][j].y]=false;
-					}
+		//cleanup
+		for(int l_c=0;l_c<2;l_c++){
+			for(int i_c=0;i_c<x_size;i_c++){
+				for(int j_c=0;j_c<y_size;j_c++){
+					grid[l_c][i_c][j_c].is_reached=false;
 				}
 			}
-			fout<<"0"<<endl;
 		}
-		
+		cout<<"Route of net " <<i+1<<" completed"<<endl;
+		while(!wavefront.empty())
+			wavefront.pop();		
 	}
-	else cout<<"Unable to open file"<<endl;
+	ofstream fout(outputfile);
+	
+	fout<<nnz<<endl;
+
+	for(int i=0;i<nnz;i++){
+		fout<<i+1<<endl; 
+		if(netout[i].size()!=2){
+			for(int j=netout[i].size()-1;j>=0;j--){
+				fout<<netout[i][j].lay_no+1<<" "<<netout[i][j].x<<" "<<netout[i][j].y<<endl;
+				if(via[netout[i][j].x][netout[i][j].y]){
+					fout<< "3 "<<netout[i][j].x<<" "<<netout[i][j].y<<endl;
+					via[netout[i][j].x][netout[i][j].y]=false;
+				}
+			}
+		}
+		fout<<"0"<<endl;
+	}
+	
+}
+
 	
 }
